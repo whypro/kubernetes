80cc038fc8eaeb34aa7d4474eef5cd53412e7523
Yecheng Fu <fuyecheng@qiniu.com>
2018-12-10 20:32:08 +0800

    enable unit test on travis

--- hack/travis.sh
+++ hack/travis.sh
@@ -4,21 +4,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# build linux/amd64 only
-export KUBE_FASTBUILD=true
-
 LOGFILE=/tmp/make.$TARGET.log
+
+rm -f $LOGFILE
+
+# We need to redirect logs to files to work around traivs 4MB log limit, see
+# https://docs.travis-ci.com/user/common-build-problems/#Log-Length-exceeded.
+dump_output() {
+    test -f $LOGFILE && tail -500 $LOGFILE
+}
+
+trap 'dump_output' EXIT
 
 if [[ "$TARGET" == "test-integration" ]]; then
     ./hack/install-etcd.sh
 fi
 
-dump_output() {
-    tail -500 $LOGFILE
-}
+args=(
+    $TARGET
+    # CPU resources in travis is very low, slow down to make sure tests will
+    # not timeout.
+    # See https://docs.travis-ci.com/user/reference/overview/.
+    PARALLEL=1
+)
 
-trap 'dump_output' EXIT
+# pass all KUBE_ environments
+while IFS='=' read -r -d '' n v; do
+    if [[ $n =~ ^KUBE_ ]]; then
+        args+=($n="$v")
+    fi
+done < <(env -0)
 
-# We need to redirect logs to files to work around traivs 4MB log limit, see
-# https://docs.travis-ci.com/user/common-build-problems/#Log-Length-exceeded.
-./build/run.sh make $TARGET &> $LOGFILE
+./build/run.sh make "${args[@]}" &> $LOGFILE
