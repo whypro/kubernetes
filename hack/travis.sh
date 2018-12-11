bf2e64209e64b05b24ee82d4fc1d1c5cbc3f021d
Yecheng Fu <fuyecheng@qiniu.com>
2018-12-11 17:22:52 +0800

    keep .travis.yaml simple because travis don't update travis configuration on old PR build jobs but it will fetch up-to-date branch

--- hack/travis.sh
+++ hack/travis.sh
@@ -11,7 +11,17 @@ rm -f $LOGFILE
 # We need to redirect logs to files to work around traivs 4MB log limit, see
 # https://docs.travis-ci.com/user/common-build-problems/#Log-Length-exceeded.
 dump_output() {
-    test -f $LOGFILE && tail -500 $LOGFILE
+    if [ ! -f $LOGFILE ]; then
+        return
+    fi
+    local logfile_bytes=$(du -b $LOGFILE | awk '{print $1}')
+    local bytes=$((4 * 1024 * 1024 - 50 * 1024))
+    if [[ "$bytes" -ge "$logfile_bytes" ]]; then
+        echo ">>> log file total $logfile_bytes bytes, show all contents:"
+    else
+        echo ">>> log file total $logfile_bytes bytes, show last $bytes bytes:"
+    fi
+    tail -c $bytes $LOGFILE
 }
 
 trap 'dump_output' EXIT
@@ -22,11 +32,17 @@ fi
 
 args=(
     $TARGET
-    # CPU resources in travis is very low, slow down to make sure tests will
-    # not timeout.
-    # See https://docs.travis-ci.com/user/reference/overview/.
-    PARALLEL=1
 )
+
+if [ -n "${TRAVIS:-}" ]; then
+    # Resource in travis is low, slow down and increase timeout.
+    # See https://docs.travis-ci.com/user/reference/overview/.
+    args+=(PARALLEL=1)
+    export KUBE_TIMEOUT=${KUBE_TIMEOUT:-"--timeout 300s"}
+    # Don't build for all platforms, because it may exceeed travis 2 hours
+    # limit.
+    export KUBE_FASTBUILD=true 
+fi
 
 # pass all KUBE_ environments
 while IFS='=' read -r -d '' n v; do
