a441a61fdebd8f36b1df36c96002fec3225240bd
shaloulcy <lcy041536@gmail.com>
2020-02-05 12:08:14 +0800

    validate storage cache indexers
    Signed-off-by: shaloulcy <lcy041536@gmail.com>

--- staging/src/k8s.io/apiserver/pkg/registry/generic/registry/store_test.go
+++ staging/src/k8s.io/apiserver/pkg/registry/generic/registry/store_test.go
@@ -54,6 +54,7 @@ import (
 	"k8s.io/apiserver/pkg/storage/names"
 	"k8s.io/apiserver/pkg/storage/storagebackend/factory"
 	storagetesting "k8s.io/apiserver/pkg/storage/testing"
+	"k8s.io/client-go/tools/cache"
 )
 
 var scheme = runtime.NewScheme()
@@ -1944,5 +1945,55 @@ func TestPreconditionalUpdateWithCachedObject(t *testing.T) {
 	}
 	if r.Spec.NodeName != "machine2" {
 		t.Fatalf("unexpected, update didn't take effect: %#v", r)
+	}
+}
+
+func emptyIndexFunc(obj interface{}) ([]string, error) {
+	return []string{}, nil
+}
+
+func TestValidateIndexers(t *testing.T) {
+	testcases := []struct {
+		name          string
+		indexers      *cache.Indexers
+		expectedError bool
+	}{
+		{
+			name:          "nil indexers",
+			indexers:      nil,
+			expectedError: false,
+		},
+		{
+			name: "normal indexers",
+			indexers: &cache.Indexers{
+				"f:spec.nodeName":            emptyIndexFunc,
+				"l:controller-revision-hash": emptyIndexFunc,
+			},
+			expectedError: false,
+		},
+		{
+			name: "too short indexers",
+			indexers: &cache.Indexers{
+				"f": emptyIndexFunc,
+			},
+			expectedError: true,
+		},
+		{
+			name: "invalid indexers",
+			indexers: &cache.Indexers{
+				"spec.nodeName": emptyIndexFunc,
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, tc := range testcases {
+		err := validateIndexers(tc.indexers)
+		if tc.expectedError && err == nil {
+			t.Errorf("%v: expected error, but got nil", tc.name)
+		}
+		if !tc.expectedError && err != nil {
+			t.Errorf("%v: expected no error, but got %v", tc.name, err)
+		}
 	}
 }
