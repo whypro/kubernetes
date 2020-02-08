59883be66ec5da040083cf97f8daf9840fb26300
shaloulcy <lcy041536@gmail.com>
2020-02-08 10:13:20 +0800

    add index for pod cacher
    Signed-off-by: shaloulcy <lcy041536@gmail.com>

--- pkg/registry/core/pod/storage/storage.go
+++ pkg/registry/core/pod/storage/storage.go
@@ -82,6 +82,7 @@ func NewStorage(optsGetter generic.RESTOptionsGetter, k client.ConnectionInfoGet
 		RESTOptions: optsGetter,
 		AttrFunc:    pod.GetAttrs,
 		TriggerFunc: map[string]storage.IndexerFunc{"spec.nodeName": pod.NodeNameTriggerFunc},
+		Indexers:    pod.Indexers(),
 	}
 	if err := store.CompleteWithOptions(options); err != nil {
 		panic(err) // TODO: Propagate error up
--- pkg/registry/core/pod/strategy_test.go
+++ pkg/registry/core/pod/strategy_test.go
@@ -18,6 +18,7 @@ package pod
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"reflect"
 	"testing"
@@ -30,6 +31,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+	"k8s.io/client-go/tools/cache"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
 	api "k8s.io/kubernetes/pkg/apis/core"
 	"k8s.io/kubernetes/pkg/kubelet/client"
@@ -454,5 +456,45 @@ func TestPortForwardLocation(t *testing.T) {
 		if !reflect.DeepEqual(loc, tc.expectedURL) {
 			t.Errorf("expected %v, got %v", tc.expectedURL, loc)
 		}
+	}
+}
+
+func TestPodIndexFunc(t *testing.T) {
+	tcs := []struct {
+		name          string
+		indexFunc     cache.IndexFunc
+		pod           interface{}
+		expectedValue string
+		expectedErr   error
+	}{
+		{
+			name:      "node name index",
+			indexFunc: NodeNameIndexFunc,
+			pod: &api.Pod{
+				Spec: api.PodSpec{
+					NodeName: "test-pod",
+				},
+			},
+			expectedValue: "test-pod",
+			expectedErr:   nil,
+		},
+		{
+			name:          "not a pod failed",
+			indexFunc:     NodeNameIndexFunc,
+			pod:           "not a pod object",
+			expectedValue: "test-pod",
+			expectedErr:   fmt.Errorf("not a pod"),
+		},
+	}
+
+	for _, tc := range tcs {
+		indexValues, err := tc.indexFunc(tc.pod)
+		if !reflect.DeepEqual(err, tc.expectedErr) {
+			t.Errorf("name %v, expected %v, got %v", tc.name, tc.expectedErr, err)
+		}
+		if err == nil && len(indexValues) != 1 && indexValues[0] != tc.expectedValue {
+			t.Errorf("name %v, expected %v, got %v", tc.name, tc.expectedValue, indexValues)
+		}
+
 	}
 }
