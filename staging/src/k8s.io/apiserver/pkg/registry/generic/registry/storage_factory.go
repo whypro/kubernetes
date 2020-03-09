kubernetes/apiserver

Rename TriggerPublisherFunc as IndexerFunc
Kubernetes-commit: 6e98aab26e4fb867a6d8b4342937823cf1597d89

pkg/registry/generic/registry/storage_factory.go
@@ -39,7 +39,7 @@ func StorageWithCacher(capacity int) generic.StorageDecorator {
 		newFunc func() runtime.Object,
 		newListFunc func() runtime.Object,
 		getAttrsFunc storage.AttrFunc,
-		triggerFuncs storage.TriggerPublisherFuncs) (storage.Interface, factory.DestroyFunc, error) {
+		triggerFuncs storage.IndexerFuncs) (storage.Interface, factory.DestroyFunc, error) {
 
 		s, d, err := generic.NewRawStorage(storageConfig)
 		if err != nil {
@@ -56,16 +56,16 @@ func StorageWithCacher(capacity int) generic.StorageDecorator {
 		// TODO: we would change this later to make storage always have cacher and hide low level KV layer inside.
 		// Currently it has two layers of same storage interface -- cacher and low level kv.
 		cacherConfig := cacherstorage.Config{
-			CacheCapacity:         capacity,
-			Storage:               s,
-			Versioner:             etcd3.APIObjectVersioner{},
-			ResourcePrefix:        resourcePrefix,
-			KeyFunc:               keyFunc,
-			NewFunc:               newFunc,
-			NewListFunc:           newListFunc,
-			GetAttrsFunc:          getAttrsFunc,
-			TriggerPublisherFuncs: triggerFuncs,
-			Codec:                 storageConfig.Codec,
+			CacheCapacity:  capacity,
+			Storage:        s,
+			Versioner:      etcd3.APIObjectVersioner{},
+			ResourcePrefix: resourcePrefix,
+			KeyFunc:        keyFunc,
+			NewFunc:        newFunc,
+			NewListFunc:    newListFunc,
+			GetAttrsFunc:   getAttrsFunc,
+			IndexerFuncs:   triggerFuncs,
+			Codec:          storageConfig.Codec,
 		}
 		cacher, err := cacherstorage.NewCacherFromConfig(cacherConfig)
 		if err != nil {

whypro/kubernetes

staging/src/k8s.io/apiserver/pkg/registry/generic/registry/storage_factory.go
@@ -39,7 +39,7 @@ func StorageWithCacher(capacity int) generic.StorageDecorator {
 		newFunc func() runtime.Object,
 		newListFunc func() runtime.Object,
 		getAttrsFunc storage.AttrFunc,
-		triggerFuncs storage.TriggerPublisherFuncs) (storage.Interface, factory.DestroyFunc, error) {
+		triggerFuncs storage.IndexerFuncs) (storage.Interface, factory.DestroyFunc, error) {
 
 		s, d, err := generic.NewRawStorage(storageConfig)
 		if err != nil {
@@ -56,16 +56,16 @@ func StorageWithCacher(capacity int) generic.StorageDecorator {
 		// TODO: we would change this later to make storage always have cacher and hide low level KV layer inside.
 		// Currently it has two layers of same storage interface -- cacher and low level kv.
 		cacherConfig := cacherstorage.Config{
-			CacheCapacity:         capacity,
-			Storage:               s,
-			Versioner:             etcdstorage.APIObjectVersioner{},
-			ResourcePrefix:        resourcePrefix,
-			KeyFunc:               keyFunc,
-			NewFunc:               newFunc,
-			NewListFunc:           newListFunc,
-			GetAttrsFunc:          getAttrsFunc,
-			TriggerPublisherFuncs: triggerFuncs,
-			Codec:                 storageConfig.Codec,
+			CacheCapacity:  capacity,
+			Storage:        s,
+			Versioner:      etcdstorage.APIObjectVersioner{},
+			ResourcePrefix: resourcePrefix,
+			KeyFunc:        keyFunc,
+			NewFunc:        newFunc,
+			NewListFunc:    newListFunc,
+			GetAttrsFunc:   getAttrsFunc,
+			IndexerFuncs:   triggerFuncs,
+			Codec:          storageConfig.Codec,
 		}
 		cacher, err := cacherstorage.NewCacherFromConfig(cacherConfig)
 		if err != nil {